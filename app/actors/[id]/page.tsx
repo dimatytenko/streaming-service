566c3cdcf631c5924065f17fac15649a2620401b
Dima Tytenko <amgpetronass@gmail.com>
2023-07-10 21:44:58 +0300

    fix

--- app/actors/[id]/page.tsx
+++ app/actors/[id]/page.tsx
@@ -18,14 +18,13 @@ export const generateMetadata = ({params: {id}}: Props) => {
 export default async function Movie({params: {id}}: Props) {
   const actor = await getActorById(id);
   const movies = await getActorMovies(id);
-  console.log('movies', movies);
   const tvShows = await getActorTvShows(id);
   const images = await getActorImages(id);
 
   return (
     <ActorsWrapper>
       <ActorsCardContent card={actor} />
-      <ActorTabs title={'hwvecwjkhckjew'} movies={movies} shows={tvShows} images={images} />
+      <ActorTabs title={'Actor`s activity'} movies={movies} shows={tvShows} images={images} />
     </ActorsWrapper>
   );
 }
