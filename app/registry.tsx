b7070e10b82a25526fe08a9acb2c83f6919d32b3
Dima Tytenko <amgpetronass@gmail.com>
2023-07-10 19:21:39 +0300

    fix

--- app/registry.tsx
+++ app/registry.tsx
@@ -11,6 +11,7 @@ export default function StyledComponentsRegistry({children}: {children: React.Re
 
   useServerInsertedHTML(() => {
     const styles = styledComponentsStyleSheet.getStyleElement();
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     styledComponentsStyleSheet.instance.clearTag();
     return styles;
